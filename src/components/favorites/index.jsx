3b505dcd3496367d0ec08b77467c01ff50de8afc
Mohosin999 <hasanmohosin998@gmail.com>
2023-02-16 18:44:48 +0600

    not work perfectly

--- src/components/favorites/index.jsx
+++ src/components/favorites/index.jsx
@@ -9,39 +9,30 @@ const Favorites = () => {
   const { items } = useStoreState((state) => state.favorites);
   const itemId = items[items.length - 1];
 
-  // if (data[itemId]) {
-  //   const obj = data[itemId];
-  //   console.log(obj);
-  // } else {
-  //   console.log(null);
-
   const itemObj = data[itemId];
-  const itemArray = Object.entries(itemObj);
-  console.log(itemArray);
 
-  // const { removeFromFavorite } = useStoreActions(
-  //   (actions) => actions.favorites
-  // );
+  const { removeFromFavorite } = useStoreActions(
+    (actions) => actions.favorites
+  );
 
   return (
     <div>
-      {/* <Container maxWidth={"lg"} sx={{ marginTop: 12 }}>
-        {itemArray.length > 0 && (
-          <Grid container alignItems="stretch">
-            {itemArray.map((item) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} mb={2}>
-                <PlaylistCardItem
-                  key={item.playlistId}
-                  playlistId={item.playlistId}
-                  playlistThumbnail={item.playlistThumbnail}
-                  playlistTitle={item.playlistTitle}
-                  channelTitle={item.channelTitle}
-                />
-              </Grid>
-            ))}
+      <Container maxWidth={"lg"} sx={{ marginTop: 12 }}>
+        <Grid container alignItems="stretch">
+          <Grid item xs={12} sm={6} md={4} lg={3} mb={2}>
+            <PlaylistCardItem
+              key={itemObj.playlistId}
+              playlistId={itemObj.playlistId}
+              playlistThumbnail={itemObj.playlistThumbnail}
+              playlistTitle={itemObj.playlistTitle}
+              channelTitle={itemObj.channelTitle}
+            />
+            <button onClick={() => removeFromFavorite(itemObj.playlistId)}>
+              Remove From Favorite
+            </button>
           </Grid>
-        )}
-      </Container> */}
+        </Grid>
+      </Container>
     </div>
   );
 };
