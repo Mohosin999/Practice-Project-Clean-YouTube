cc262ebf3fa055d2725eb4f0d7625159de843cf9
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-08-13 12:35:32 +0600

    feature: added pagination
    in home and favorite page, pagination added successfully

--- src/components/pages/favorite-page/index.jsx
+++ src/components/pages/favorite-page/index.jsx
@@ -2,7 +2,7 @@
 // import { useStoreState, useStoreActions } from "easy-peasy";
 // import { Container } from "@mui/system";
 // import PlaylistCardItem from "../../playlist-card-item";
-// import { Grid } from "@mui/material";
+// import { Box, Grid, Typography } from "@mui/material";
 
 // const Favorites = () => {
 //   const { data } = useStoreState((state) => state.playlists);
@@ -13,7 +13,7 @@
 //   return (
 //     <div>
 //       <Container maxWidth={"lg"} sx={{ paddingTop: 12 }}>
-//         {itemArray.length > 0 && (
+//         {itemArray.length > 0 ? (
 //           <Grid container alignItems="stretch">
 //             {itemArray.map((item) => (
 //               <Grid item xs={12} sm={6} md={4} lg={3} mb={2}>
@@ -28,6 +28,25 @@
 //               </Grid>
 //             ))}
 //           </Grid>
+//         ) : (
+//           <Box
+//             sx={{
+//               display: "flex",
+//               flexDirection: "column",
+//               alignItems: "center",
+//               justifyContent: "center",
+//               minHeight: "50vh",
+//               textAlign: "center",
+//               color: "text.secondary",
+//             }}
+//           >
+//             <Typography variant="h4" gutterBottom>
+//               🗳️ Empty Favorite Page
+//             </Typography>
+//             <Typography variant="body1">
+//               Start adding your favorite playlists to see them here!
+//             </Typography>
+//           </Box>
 //         )}
 //       </Container>
 //     </div>
@@ -36,27 +55,51 @@
 
 // export default Favorites;
 
-import React from "react";
-import { useStoreState, useStoreActions } from "easy-peasy";
+import React, { useState } from "react";
+import { useStoreState } from "easy-peasy";
 import { Container } from "@mui/system";
 import PlaylistCardItem from "../../playlist-card-item";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Pagination } from "@mui/material";
 
 const Favorites = () => {
   const { data } = useStoreState((state) => state.playlists);
   const { items } = useStoreState((state) => state.favorites);
-  const itemArray = [];
-  items.forEach((item) => itemArray.push(data[item]));
+
+  const itemArray = items.map((item) => data[item]);
+
+  // Pagination state
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+
+  // Calculate the number of pages
+  const totalPages = Math.ceil(itemArray.length / itemsPerPage);
+
+  // Calculate the index of the first item on the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = itemArray.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Handle page change
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
 
   return (
-    <div>
-      <Container maxWidth={"lg"} sx={{ paddingTop: 12 }}>
-        {itemArray.length > 0 ? (
-          <Grid container alignItems="stretch">
-            {itemArray.map((item) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} mb={2}>
+    <Container maxWidth={"lg"} sx={{ paddingTop: 12 }}>
+      {itemArray.length > 0 ? (
+        <>
+          <Grid container alignItems="stretch" spacing={2} sx={{ flex: 1 }}>
+            {currentItems.map((item) => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                mb={2}
+                key={item.playlistId}
+              >
                 <PlaylistCardItem
-                  key={item.playlistId}
                   playlistId={item.playlistId}
                   playlistThumbnail={item.playlistThumbnail}
                   playlistTitle={item.playlistTitle}
@@ -66,28 +109,38 @@ const Favorites = () => {
               </Grid>
             ))}
           </Grid>
-        ) : (
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              minHeight: "50vh",
-              textAlign: "center",
-              color: "text.secondary",
-            }}
-          >
-            <Typography variant="h4" gutterBottom>
-              🗳️ Empty Favorite Page
-            </Typography>
-            <Typography variant="body1">
-              Start adding your favorite playlists to see them here!
-            </Typography>
-          </Box>
-        )}
-      </Container>
-    </div>
+          {totalPages > 1 && (
+            <div style={{ display: "flex", justifyContent: "end" }}>
+              <Pagination
+                count={totalPages}
+                page={currentPage}
+                onChange={handlePageChange}
+                color={"primary"}
+              />
+            </div>
+          )}
+        </>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "50vh",
+            textAlign: "center",
+            color: "text.secondary",
+          }}
+        >
+          <Typography variant="h4" gutterBottom>
+            🗳️ Empty Favorite Page
+          </Typography>
+          <Typography variant="body1">
+            Start adding your favorite playlists to see them here!
+          </Typography>
+        </Box>
+      )}
+    </Container>
   );
 };
 
--- src/components/pages/home-page/index.jsx
+++ src/components/pages/home-page/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStoreState } from "easy-peasy";
-import { Grid } from "@mui/material";
+import { Grid, Pagination } from "@mui/material";
 import { Container } from "@mui/system";
 import PlaylistCardItem from "../../playlist-card-item";
 
@@ -8,24 +8,64 @@ const HomePage = () => {
   const { data } = useStoreState((state) => state.playlists);
   const playlistArray = Object.values(data);
 
+  // Pagination state
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+
+  // Calculate the number of pages
+  const totalPages = Math.ceil(playlistArray.length / itemsPerPage);
+
+  // Calculate the index of the first item on the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentPlaylists = playlistArray.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  // Handle page change
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <>
       <Container maxWidth={"lg"} sx={{ paddingTop: 12 }}>
         {playlistArray.length > 0 && (
-          <Grid container alignItems="stretch">
-            {playlistArray.map((item) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} mb={2}>
-                <PlaylistCardItem
+          <>
+            <Grid container alignItems="stretch">
+              {currentPlaylists.map((item) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  lg={3}
+                  mb={2}
                   key={item.playlistId}
-                  playlistId={item.playlistId}
-                  playlistThumbnail={item.playlistThumbnail}
-                  playlistTitle={item.playlistTitle}
-                  channelTitle={item.channelTitle}
-                  path={"home"}
+                >
+                  <PlaylistCardItem
+                    playlistId={item.playlistId}
+                    playlistThumbnail={item.playlistThumbnail}
+                    playlistTitle={item.playlistTitle}
+                    channelTitle={item.channelTitle}
+                    path={"home"}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+            {totalPages > 1 && (
+              <div style={{ display: "flex", justifyContent: "end" }}>
+                <Pagination
+                  count={totalPages}
+                  page={currentPage}
+                  onChange={handlePageChange}
+                  color={"primary"}
+                  sx={{ marginTop: 2 }}
                 />
-              </Grid>
-            ))}
-          </Grid>
+              </div>
+            )}
+          </>
         )}
       </Container>
     </>
