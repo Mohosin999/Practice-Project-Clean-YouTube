fa35d9423c969870ba9f30abd428af90769f1019
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-11-29 08:37:54 +0600

    style: for video player page

--- src/components/video-player/index.jsx
+++ src/components/video-player/index.jsx
@@ -1,12 +1,133 @@
+// import React from "react";
+// import { useStoreState } from "easy-peasy";
+// import YouTube from "react-youtube";
+// import { useLocation } from "react-router-dom";
+// import { Container } from "@mui/system";
+// import useMediaQuery from "@mui/material/useMediaQuery";
+// import { useTheme } from "@mui/material/styles";
+// import { Button } from "@mui/material";
+// import { Link } from "react-router-dom";
+
+// const VideoPlayer = () => {
+//   const { data } = useStoreState((state) => state.playlists);
+
+//   const location = useLocation();
+//   const videoId = new URLSearchParams(location.search).get("videoId");
+//   const playlistId = location.pathname.split("/")[2];
+//   const index = location.pathname.split("/")[3];
+//   const playlistItems = data[playlistId].playlistItems;
+//   const lastItem = playlistItems.length - 1;
+
+//   // Previous button logic
+//   const prevIndex = parseInt(index) - 1;
+//   const prevItem = playlistItems[prevIndex];
+//   const prevVideoId = prevItem ? prevItem.contentDetails.videoId : "";
+
+//   // Next button logic
+//   const nextIndex = parseInt(index) + 1;
+//   const nextItem = playlistItems[nextIndex];
+//   const nextVideoId = nextItem ? nextItem.contentDetails.videoId : "";
+
+//   // Responsive design
+//   const theme = useTheme();
+//   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+//   const opts = {
+//     playerVars: {
+//       autoplay: 1,
+//       fullscreen: 1,
+//     },
+//     height: isSmallScreen ? "200" : "500",
+//     width: "100%",
+//   };
+
+//   const onPlayerReady = (event) => {
+//     event.target.playVideo();
+//   };
+
+//   return (
+//     <Container maxWidth={"md"}>
+//       <div
+//         style={{
+//           position: "relative",
+//           paddingBottom: isSmallScreen ? "56.25%" : "0",
+//           height: isSmallScreen ? "0" : "500px",
+//           marginTop: "64px",
+//         }}
+//       >
+//         <YouTube
+//           videoId={videoId}
+//           opts={opts}
+//           onReady={onPlayerReady}
+//           style={{
+//             position: isSmallScreen ? "absolute" : "static",
+//             top: 0,
+//             left: 0,
+//             width: isSmallScreen ? "100%" : "auto",
+//             height: isSmallScreen ? "100%" : "500px",
+//           }}
+//         />
+//         <div
+//           style={{
+//             display: "flex",
+//             justifyContent: "flex-end",
+//             marginTop: "0.2rem",
+//           }}
+//         >
+//           {/* Previous Video Button */}
+//           <Button
+//             to={`/player/${playlistId}/${prevIndex}?videoId=${prevVideoId}`}
+//             component={Link}
+//             variant="contained"
+//             color="success"
+//             disabled={parseInt(index) === 0}
+//           >
+//             Previous
+//           </Button>
+
+//           {/* Close Running Video Button */}
+//           <Button
+//             to={`/player/${playlistId}`}
+//             component={Link}
+//             variant="contained"
+//             color="error"
+//             sx={{ marginLeft: "0.2rem" }}
+//           >
+//             Close Video
+//           </Button>
+
+//           {/* Next Video Button */}
+//           <Button
+//             to={`/player/${playlistId}/${nextIndex}?videoId=${nextVideoId}`}
+//             component={Link}
+//             variant="contained"
+//             color="success"
+//             disabled={parseInt(index) === lastItem}
+//             sx={{ marginLeft: "0.2rem" }}
+//           >
+//             Next
+//           </Button>
+//         </div>
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default VideoPlayer;
+
 import React from "react";
 import { useStoreState } from "easy-peasy";
 import YouTube from "react-youtube";
 import { useLocation } from "react-router-dom";
-import { Container } from "@mui/system";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { useTheme } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import {
+  Container,
+  Box,
+  IconButton,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import { ArrowBack, ArrowForward, Close } from "@mui/icons-material";
 
 const VideoPlayer = () => {
   const { data } = useStoreState((state) => state.playlists);
@@ -46,13 +167,15 @@ const VideoPlayer = () => {
   };
 
   return (
-    <Container maxWidth={"md"}>
-      <div
-        style={{
+    <Container maxWidth="lg" sx={{ mt: 12, mb: 5 }}>
+      <Box
+        sx={{
           position: "relative",
           paddingBottom: isSmallScreen ? "56.25%" : "0",
           height: isSmallScreen ? "0" : "500px",
-          marginTop: "64px",
+          borderRadius: "8px",
+          overflow: "hidden",
+          boxShadow: theme.shadows[3],
         }}
       >
         <YouTube
@@ -67,48 +190,73 @@ const VideoPlayer = () => {
             height: isSmallScreen ? "100%" : "500px",
           }}
         />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            marginTop: "0.2rem",
+      </Box>
+
+      {/* Navigation Buttons */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          mt: 3,
+          flexDirection: "row", // Always in a row
+          gap: 2, // Consistent spacing between buttons
+          width: "100%",
+        }}
+      >
+        {/* Previous Button */}
+        <IconButton
+          to={`/player/${playlistId}/${prevIndex}?videoId=${prevVideoId}`}
+          component={Link}
+          disabled={parseInt(index) === 0}
+          size={isSmallScreen ? "small" : "medium"} // Smaller buttons on small screens
+          sx={{
+            backgroundColor: "gold", // Gold background color
+            color: "black", // Black icon color for better contrast
+            "&:hover": { backgroundColor: "#ffc107" }, // Darker gold on hover
+            "&:disabled": { backgroundColor: "grey.400", color: "grey.600" }, // Grayed-out for disabled state
+            width: isSmallScreen ? "36px" : "48px", // Adjust button size for small screens
+            height: isSmallScreen ? "36px" : "48px", // Maintain circular shape
           }}
         >
-          {/* Previous Video Button */}
-          <Button
-            to={`/player/${playlistId}/${prevIndex}?videoId=${prevVideoId}`}
-            component={Link}
-            variant="contained"
-            color="success"
-            disabled={parseInt(index) === 0}
-          >
-            Previous
-          </Button>
+          <ArrowBack fontSize={isSmallScreen ? "small" : "medium"} />{" "}
+          {/* Icon size adjusts */}
+        </IconButton>
 
-          {/* Close Running Video Button */}
-          <Button
-            to={`/player/${playlistId}`}
-            component={Link}
-            variant="contained"
-            color="error"
-            sx={{ marginLeft: "0.2rem" }}
-          >
-            Close Video
-          </Button>
+        {/* Close Video Button */}
+        <IconButton
+          to={`/player/${playlistId}`}
+          component={Link}
+          size={isSmallScreen ? "small" : "medium"}
+          sx={{
+            backgroundColor: "gold",
+            color: "black",
+            "&:hover": { backgroundColor: "#ffc107" },
+            width: isSmallScreen ? "36px" : "48px",
+            height: isSmallScreen ? "36px" : "48px",
+          }}
+        >
+          <Close fontSize={isSmallScreen ? "small" : "medium"} />
+        </IconButton>
 
-          {/* Next Video Button */}
-          <Button
-            to={`/player/${playlistId}/${nextIndex}?videoId=${nextVideoId}`}
-            component={Link}
-            variant="contained"
-            color="success"
-            disabled={parseInt(index) === lastItem}
-            sx={{ marginLeft: "0.2rem" }}
-          >
-            Next
-          </Button>
-        </div>
-      </div>
+        {/* Next Button */}
+        <IconButton
+          to={`/player/${playlistId}/${nextIndex}?videoId=${nextVideoId}`}
+          component={Link}
+          disabled={parseInt(index) === lastItem}
+          size={isSmallScreen ? "small" : "medium"}
+          sx={{
+            backgroundColor: "gold",
+            color: "black",
+            "&:hover": { backgroundColor: "#ffc107" },
+            "&:disabled": { backgroundColor: "grey.400", color: "grey.600" },
+            width: isSmallScreen ? "36px" : "48px",
+            height: isSmallScreen ? "36px" : "48px",
+          }}
+        >
+          <ArrowForward fontSize={isSmallScreen ? "small" : "medium"} />
+        </IconButton>
+      </Box>
     </Container>
   );
 };
