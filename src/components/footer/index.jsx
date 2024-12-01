0541665976d3786fbec300378e22bca4b025d3ad
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-12-01 09:09:47 +0600

    footer style redesigned

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -37,149 +37,162 @@ const Footer = () => {
         marginTop: 4,
         textAlign: "center",
         display: "flex",
-        flexDirection: isMediumScreen ? "column" : "row",
-        justifyContent: "space-between",
+        flexDirection: "column",
         alignItems: "center",
-        gap: isMediumScreen ? 2 : 0,
+        gap: 2,
       }}
     >
-      {/* Left Section */}
-      <Typography
-        variant="body2"
-        sx={{
-          fontSize: isMediumScreen ? "0.8rem" : "1rem",
-        }}
-      >
-        © {new Date().getFullYear()} Clean YouTube
-      </Typography>
-
-      {/* Center Email Section */}
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
+          flexDirection: isMediumScreen ? "column" : "row",
+          justifyContent: "space-between",
           alignItems: "center",
-          textAlign: "center",
+          gap: isMediumScreen ? 2 : 0,
+          width: "100%",
+          borderBottom: "1px solid #fff",
+          paddingBottom: 2,
         }}
       >
-        <Typography
-          variant="body1"
-          sx={{
-            fontSize: "1rem",
-            fontWeight: "bold",
-            marginBottom: 1,
-          }}
-        >
-          Email me for quick contact
-        </Typography>
+        {/* Center Email Section */}
         <Box
           sx={{
             display: "flex",
-            flexDirection: isMediumScreen ? "column" : "row",
+            flexDirection: "column",
             alignItems: "center",
-            gap: 1,
-            border: "1px solid #fff",
-            padding: "12px 24px",
-            borderRadius: "8px",
-            background: "rgba(255, 255, 255, 0.1)",
+            textAlign: "center",
           }}
         >
           <Typography
             variant="body1"
             sx={{
               fontSize: "1rem",
-              wordBreak: "break-word",
+              fontWeight: "bold",
+              marginBottom: 1,
             }}
           >
-            {email}
+            Quick Contact via Email
           </Typography>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="small"
-            onClick={handleCopyEmail}
+          <Box
             sx={{
-              fontWeight: "bold",
-              backgroundColor: isCopied ? "#4caf50" : "#1E90FF",
+              display: "flex",
+              flexDirection: isMediumScreen ? "column" : "row",
+              alignItems: "center",
+              gap: 1,
+              border: "1px solid #fff",
+              padding: "12px 24px",
+              borderRadius: "8px",
+              background: "rgba(255, 255, 255, 0.1)",
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontSize: "1rem",
+                wordBreak: "break-word",
+              }}
+            >
+              {email}
+            </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="small"
+              onClick={handleCopyEmail}
+              sx={{
+                fontWeight: "bold",
+                backgroundColor: isCopied ? "#4caf50" : "#1E90FF",
+                color: "#fff",
+                borderRadius: "20px",
+                padding: "4px 10px",
+                fontSize: "0.9rem",
+                "&:hover": {
+                  backgroundColor: isCopied ? "#388e3c" : "#1C86EE",
+                },
+              }}
+            >
+              {isCopied ? "Copied" : "Copy"}
+            </Button>
+          </Box>
+        </Box>
+
+        {/* Right Social Media Section */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <IconButton
+            href="https://www.facebook.com/mohosinh99"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
               color: "#fff",
-              borderRadius: "20px",
-              padding: "4px 10px",
-              fontSize: "0.9rem",
               "&:hover": {
-                backgroundColor: isCopied ? "#388e3c" : "#1C86EE",
+                transform: "scale(1.2)",
+                transition: "transform 0.2s ease-in-out",
               },
             }}
           >
-            {isCopied ? "Copied" : "Copy"}
-          </Button>
+            <Facebook />
+          </IconButton>
+          <IconButton
+            href="https://x.com/mohosinh99"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
+              color: "#fff",
+              "&:hover": {
+                transform: "scale(1.2)",
+                transition: "transform 0.2s ease-in-out",
+              },
+            }}
+          >
+            <Twitter />
+          </IconButton>
+          <IconButton
+            href="https://www.linkedin.com/in/mohosinh99/"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
+              color: "#fff",
+              "&:hover": {
+                transform: "scale(1.2)",
+                transition: "transform 0.2s ease-in-out",
+              },
+            }}
+          >
+            <LinkedIn />
+          </IconButton>
+          <IconButton
+            href="https://github.com/Mohosin999"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
+              color: "#fff",
+              "&:hover": {
+                transform: "scale(1.2)",
+                transition: "transform 0.2s ease-in-out",
+              },
+            }}
+          >
+            <GitHub />
+          </IconButton>
         </Box>
       </Box>
 
-      {/* Right Social Media Section */}
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 2,
-        }}
-      >
-        <IconButton
-          href="https://www.facebook.com/mohosinh99"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box>
+        <Typography
+          variant="body2"
           sx={{
-            color: "#4267B2",
-            "&:hover": {
-              transform: "scale(1.2)",
-              transition: "transform 0.2s ease-in-out",
-            },
+            fontSize: isMediumScreen ? "0.8rem" : "1rem",
           }}
         >
-          <Facebook />
-        </IconButton>
-        <IconButton
-          href="https://x.com/mohosinh99"
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{
-            color: "#1DA1F2",
-            "&:hover": {
-              transform: "scale(1.2)",
-              transition: "transform 0.2s ease-in-out",
-            },
-          }}
-        >
-          <Twitter />
-        </IconButton>
-        <IconButton
-          href="https://www.linkedin.com/in/mohosinh99/"
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{
-            color: "#0A66C2",
-            "&:hover": {
-              transform: "scale(1.2)",
-              transition: "transform 0.2s ease-in-out",
-            },
-          }}
-        >
-          <LinkedIn />
-        </IconButton>
-        <IconButton
-          href="https://github.com/Mohosin999"
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{
-            color: "#fff",
-            "&:hover": {
-              transform: "scale(1.2)",
-              transition: "transform 0.2s ease-in-out",
-            },
-          }}
-        >
-          <GitHub />
-        </IconButton>
+          © 2023 Clean YouTube
+        </Typography>
       </Box>
     </Box>
   );
