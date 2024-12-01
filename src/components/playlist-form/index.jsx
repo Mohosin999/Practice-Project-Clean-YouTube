4e1269b3d7b707bd59c9d16a4935d4d0dd6eba14
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-12-01 09:10:06 +0600

    added some sily changes

--- src/components/playlist-form/index.jsx
+++ src/components/playlist-form/index.jsx
@@ -1,93 +1,3 @@
-// import React, { useState } from "react";
-// import { useStoreActions, useStoreState } from "easy-peasy";
-// import Button from "@mui/material/Button";
-// import TextField from "@mui/material/TextField";
-// import Dialog from "@mui/material/Dialog";
-// import DialogActions from "@mui/material/DialogActions";
-// import DialogContent from "@mui/material/DialogContent";
-// import DialogContentText from "@mui/material/DialogContentText";
-// import DialogTitle from "@mui/material/DialogTitle";
-// import useSnackbar from "../../hooks/useSnackbar";
-
-// const PlaylistForm = ({ open, handleClose }) => {
-//   const [state, setState] = useState("");
-//   const [isError, setIsError] = useState(false);
-
-//   const { handleSnackbar, SnackbarComponent } = useSnackbar();
-
-//   const { getPlaylist } = useStoreActions((actions) => actions.playlists);
-//   const { data } = useStoreState((state) => state.playlists);
-
-//   const handleSubmit = async () => {
-//     if (!state) {
-//       setIsError(true);
-//       handleSnackbar("Invalid Link or ID", "error");
-//       return;
-//     }
-
-//     /**
-//      * This is extracting the playlist ID from a given YouTube playlist URL
-//      * - /(?:list=)([\w-]+)/)?.[1]
-//      */
-//     const playlistId = state.match(/(?:list=)([\w-]+)/)?.[1] || state;
-
-//     if (data[playlistId]) {
-//       handleSnackbar("Playlist Already Exists!", "warning");
-//       return;
-//     }
-
-//     try {
-//       setIsError(false);
-
-//       // Attempt to fetch the playlist
-//       await getPlaylist(playlistId);
-
-//       // Success handling
-//       handleSnackbar("Playlist Added Successfully", "success");
-//       setState("");
-//       handleClose();
-//     } catch (error) {
-//       // Error handling
-//       console.error("Error fetching playlist:", error);
-//       handleSnackbar(
-//         "Failed to add playlist. Please check the ID or link.",
-//         "error"
-//       );
-//     }
-//   };
-
-//   return (
-//     <>
-//       <Dialog open={open} onClose={handleClose}>
-//         <DialogTitle>Add Playlist</DialogTitle>
-//         <DialogContent>
-//           <DialogContentText>
-//             To add a playlist, please input the playlist link or ID. Otherwise,
-//             we cannot fetch the playlist.
-//           </DialogContentText>
-//           <TextField
-//             autoFocus
-//             margin="dense"
-//             label="Playlist Link or ID"
-//             fullWidth
-//             error={isError}
-//             variant="standard"
-//             onChange={(e) => setState(e.target.value)}
-//             value={state}
-//           />
-//         </DialogContent>
-//         <DialogActions>
-//           <Button onClick={handleClose}>Cancel</Button>
-//           <Button onClick={handleSubmit}>Add Playlist</Button>
-//         </DialogActions>
-//       </Dialog>
-//       <SnackbarComponent />
-//     </>
-//   );
-// };
-
-// export default PlaylistForm;
-
 import React, { useState } from "react";
 import { useStoreActions, useStoreState } from "easy-peasy";
 import Button from "@mui/material/Button";
@@ -98,6 +8,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import useSnackbar from "../../hooks/useSnackbar";
+import { Add } from "@mui/icons-material";
 
 const PlaylistForm = ({ open, handleClose }) => {
   const [state, setState] = useState("");
@@ -148,10 +59,26 @@ const PlaylistForm = ({ open, handleClose }) => {
 
   return (
     <>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add Playlist</DialogTitle>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        sx={{
+          "& .MuiPaper-root": {
+            backgroundColor: "#292929",
+          },
+        }}
+      >
+        <DialogTitle
+          sx={{
+            color: "#fff",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Add /> Add Playlist
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText>
+          <DialogContentText sx={{ color: "#B4B2B0" }}>
             To add a playlist, please input the playlist link or ID. Otherwise,
             we cannot fetch the playlist.
           </DialogContentText>
@@ -164,13 +91,33 @@ const PlaylistForm = ({ open, handleClose }) => {
             variant="standard"
             onChange={(e) => setState(e.target.value)}
             value={state}
+            InputLabelProps={{
+              style: { color: "#fff" }, // Gold label color
+            }}
+            sx={{
+              backgroundColor: "#424242", // Slightly lighter gray background
+              borderRadius: "4px",
+              input: { color: "#FFFFFF" }, // Text color in the input field
+            }}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSubmit}>Add Playlist</Button>
+          {/* <Button variant="outlined" onClick={handleClose}> */}
+          <Button
+            variant="outlined"
+            onClick={() => {
+              handleClose();
+              setState("");
+            }}
+          >
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={handleSubmit}>
+            Add Playlist
+          </Button>
         </DialogActions>
       </Dialog>
+
       <SnackbarComponent />
     </>
   );
